c8bffc608d1d080b2eecb0a24ee3adc15ec19a4f
Daniel Knuth <d-knuth@gmx.de>
2023-11-24 08:54:00 +0100

    changed DayFactory input to integer

--- src/main/java/de/dknuth/adventofcode23/Main.java
+++ src/main/java/de/dknuth/adventofcode23/Main.java
@@ -1,20 +1,22 @@
 package de.dknuth.adventofcode23;
 
-import de.dknuth.adventofcode23.day.Day;
 import de.dknuth.adventofcode23.day.DayFactory;
-import de.dknuth.adventofcode23.day01.Day01;
 
 public class Main {
     public static void main(String[] args) {
-        Class<? extends Day> day = Day01.class;
-        print(day);
+        int dayNumber = 1;
+        print(dayNumber);
     }
 
-    private static void print(Class<? extends Day> day) {
-        StringBuilder builder = new StringBuilder("\n");
-        builder.append(day.getSimpleName() + ":\n");
-        builder.append("\tPart 1: " + DayFactory.create(day).part1() + "\n");
-        builder.append("\tPart 2: " + DayFactory.create(day).part2() + "\n");
-        System.out.println(builder.toString());
+    private static void print(int dayNumber) {
+        if (DayFactory.canCreate(dayNumber)) {
+            StringBuilder builder = new StringBuilder("\n");
+            builder.append("Day " + dayNumber + ":\n");
+            builder.append("\tPart 1: " + DayFactory.create(dayNumber).part1() + "\n");
+            builder.append("\tPart 2: " + DayFactory.create(dayNumber).part2() + "\n");
+            System.out.println(builder.toString());
+        } else {
+            System.out.println("Day has not been solved yet.");
+        }
     }
 }
--- src/main/java/de/dknuth/adventofcode23/day/DayFactory.java
+++ src/main/java/de/dknuth/adventofcode23/day/DayFactory.java
@@ -1,15 +1,56 @@
 package de.dknuth.adventofcode23.day;
 
+import static java.util.Map.entry;
+
 import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+import de.dknuth.adventofcode23.day01.Day01;
 
 public class DayFactory {
 
     private DayFactory() {
     };
 
-    public static <T extends Day> Day create(Class<T> dayClass) {
+    private static final Map<Integer, Class<? extends Day>> DAY_CLASSES = Map.ofEntries(
+            entry(1, Day01.class)
+    // ,entry(2, Day02.class)
+    // ,entry(3, Day03.class)
+    // ,entry(4, Day04.class)
+    // ,entry(5, Day05.class)
+    // ,entry(6, Day06.class)
+    // ,entry(7, Day07.class)
+    // ,entry(8, Day08.class)
+    // ,entry(9, Day09.class)
+    // ,entry(10, Day10.class)
+    // ,entry(11, Day11.class)
+    // ,entry(12, Day12.class)
+    // ,entry(13, Day13.class)
+    // ,entry(14, Day14.class)
+    // ,entry(15, Day15.class)
+    // ,entry(16, Day16.class)
+    // ,entry(17, Day17.class)
+    // ,entry(18, Day18.class)
+    // ,entry(19, Day19.class)
+    // ,entry(20, Day20.class)
+    // ,entry(21, Day21.class)
+    // ,entry(22, Day22.class)
+    // ,entry(23, Day23.class)
+    // ,entry(24, Day24.class)
+    // ,entry(25, Day25.class)
+    );
+
+    public static boolean canCreate(int daynumber) {
+        return DAY_CLASSES.containsKey(daynumber);
+    }
+
+    public static Day create(int dayNumber) {
+        if (!canCreate(dayNumber)) {
+            System.out.println("Day has not been solved yet.");
+            return null;
+        }
         try {
-            Day instance = dayClass.getDeclaredConstructor().newInstance();
+            Day instance = DAY_CLASSES.get(dayNumber).getDeclaredConstructor().newInstance();
             instance.init();
             return instance;
         } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
