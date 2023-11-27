8b52e37c77c3f35bef040629457ffdb0522d5e6e
Daniel Knuth <d-knuth@gmx.de>
2023-11-27 16:39:05 +0100

    removed SuppressWarning in DayFactory

--- src/main/java/de/dknuth/adventofcode23/day/DayFactory.java
+++ src/main/java/de/dknuth/adventofcode23/day/DayFactory.java
@@ -9,31 +9,34 @@ public class DayFactory {
 
     public static boolean canCreate(int daynumber) {
         try {
-            Class.forName(numberToClassname(daynumber));
+            Class.forName(classNameOf(daynumber));
             return true;
         } catch (ClassNotFoundException e) {
             return false;
         }
     }
 
-    @SuppressWarnings("unchecked")
     public static Day create(int dayNumber) {
         try {
-            Class<? extends Day> dayClass = (Class<? extends Day>) Class.forName(numberToClassname(dayNumber));
-            Day instance = dayClass.getDeclaredConstructor().newInstance();
-            instance.init();
-            return instance;
+            Class<?> clazz = Class.forName(classNameOf(dayNumber));
+            Object instance = clazz.getDeclaredConstructor().newInstance();
+            if (instance instanceof Day) {
+                Day day = (Day) instance;
+                day.init();
+                return day;
+            }
         } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
                 | NoSuchMethodException | SecurityException | ClassCastException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             System.out.println("Day has not been solved yet.");
         }
+        System.out.println("Specified day has not implemented Day interface.");
         return null;
     }
 
-    private static String numberToClassname(int n) {
-        String number = n < 10 ? "0" + n : String.valueOf(n);
+    private static String classNameOf(int dayNumber) {
+        String number = dayNumber < 10 ? "0" + dayNumber : String.valueOf(dayNumber);
         return DayFactory.class.getPackageName() + number + ".Day" + number;
     }
 }
